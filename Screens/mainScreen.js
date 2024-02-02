233e0b4bf0f8fd77d7104b8c5c5b9b3892d77607
Brayan-asj <brayan_sj2008@hotmail.com>
2024-02-02 11:25:22 -0600

    Add more food items to the menu.

--- Screens/mainScreen.js
+++ Screens/mainScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, FlatList, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
@@ -14,6 +14,18 @@ const MainScreen = () => {
     { id: 1, name: 'Ensalada César', category: 'Ensaladas', image: require('../assets/salad.jpg') },
     { id: 2, name: 'Tiramisú', category: 'Postres', image: require('../assets/tiramisu.jpg') },
     { id: 3, name: 'Pizza Margarita', category: 'Pizzas', image: require('../assets/pizza.jpg') },
+    { id: 4, name: 'Ensalada  de Quinoa', category: 'Ensaladas', image: require('../assets/ensaladaQ.jpg') },
+    { id: 5, name: 'Ensalada  de Pollo', category: 'Ensaladas', image: require('../assets/ensaladaP.jpg') },
+    { id: 6, name: 'Ensalada   de Tomate y Cebolla', category: 'Ensaladas', image: require('../assets/ensaladaTC.jpg') },
+    { id: 7, name: 'Ensalada Atún', category: 'Ensaladas', image: require('../assets/ensaladaAT.jpg') },
+    { id: 8, name: 'Ensalada con Arroz', category: 'Ensaladas', image: require('../assets/ensaladaA.jpg') },
+    { id: 9, name: 'Pay de queso', category: 'Postres', image: require('../assets/pq.jpg') },
+    { id: 10, name: 'Pastel 3 leches', category: 'Postres', image: require('../assets/p3.jpg') },
+    { id: 11, name: 'Pay de limón', category: 'Postres', image: require('../assets/pl.jpg') },
+    { id: 12, name: 'Gelatina',  category: 'Postres', image: require('../assets/gl.jpg') },
+    { id: 13, name: 'Pizza Capriciosa', category: 'Pizzas', image: require('../assets/pc.jpg') },
+    { id: 14, name: 'Pizza Provolone', category: 'Pizzas', image: require('../assets/pp.jpg') },
+    { id: 15, name: 'Pizza Margherita', category: 'Pizzas', image: require('../assets/pm.jpg') }
     // agregar más comida por catergoría
   ];
 
